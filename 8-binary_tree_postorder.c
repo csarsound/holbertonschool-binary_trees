24de098d469d60377a6b18204f4da4f1af605236
DavidBP014 <juanbp014@gmail.com>
2022-07-28 11:35:14 -0500

    qwf

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,13 +1,14 @@
 #include "binary_trees.h"
-
+#include <stdlib.h>
 /**
- * binary_tree_delete: function that deletes an entire binary tree
- * @tree: binary tree to delete
+ * binary_tree_postorder - function that goes through a
+ * binary tree using post-order traversal
+ * @tree: is a pointer to the root node of the tree to traverse
+ * @func: is a pointer to a function to call for each node
  */
-
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if(!tree || !func)
+	if (tree == NULL || func == NULL)
 		return;
 
 	binary_tree_postorder(tree->left, func);
