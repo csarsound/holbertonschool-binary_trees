adf71a48f36b48e02c849f75b6a428348a2faf06
DavidBP014 <juanbp014@gmail.com>
2022-07-28 11:31:29 -0500

    mls

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,29 +1,33 @@
-
 #include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - check node leaf or not
+ * @node: node to check
+ * Return: 1 if node if leaf 0 if not
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	else if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
 
 /**
-* binary_tree_height - measures the height of a binary tree
-* @tree: a pointer to the root node of the tree to measure the height.
-* Return: the height of a binary tree or 0 if tree is NULL
-*/
-
+ * binary_tree_height - measures the hight of the tree
+ * @tree:pointer to the root node of the tree
+ * Return: 0 if tree is NULL else the hight
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l = 0;
-	size_t height_r = 0;
+	size_t h_left, h_right;
 
-	if (tree == NULL)
+	if (tree == NULL || binary_tree_is_leaf(tree))
 		return (0);
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
+	if (h_right <= h_left)
+		return (h_left + 1);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-
-	height_l = binary_tree_height(tree->left) + 1;
-	height_r = binary_tree_height(tree->right) + 1;
-
-	if (height_l >= height_r)
-		return (height_l);
-
-	else
-		return (height_r);
+	return (h_right + 1);
 }
